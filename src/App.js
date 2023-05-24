4c243902074785944088cb87668c87496a9fcf53
Dmitriy Angve <kirirch129@gmail.com>
2023-05-24 13:55:16 +0300

    #

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import "./App.css";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getPhotos } from "./galleryState";
 
 function App() {
-  return <div className="App"></div>;
+  // dispatch - hook allows us to call our redux actions
+  const dispatch = useDispatch();
+  // grab photos by using useSelector
+  const photos = useSelector((state) => state.gallery.photos); // gallery - keyword from "store" // "photos" - from gallerySlice in our state
+
+  useEffect(() => {
+    dispatch(getPhotos());
+  }, [dispatch]);
+  console.log(photos);
+
+  return (
+    <div className="App">
+      <h1>PHOTO GALLERY</h1>
+      <p>This is a photo gallery made using redux toolkit and redux thunk.</p>
+      <hr />
+      <div className="Gallery">
+        {/* {photos.map((photo) => <img key={} />)} */}
+      </div>
+      <button>VIEW MORE</button>
+    </div>
+  );
 }
 
 export default App;
--- src/galleryState.js
+++ src/galleryState.js
@@ -1,17 +1,41 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 export const getPhotos = createAsyncThunk("photos/getPhotos", async () => {
-  const response = await fetch("https://picsum.photos/v2/list?page=3&limit=9");
+  const apiKey = "vD2NhGZsrhvfDDRhkQKWJcinuZdI3GtLvFE1qcyjc18";
+  const apiURL = "https://api.unsplash.com/photos?page=1";
+
+  const response = await fetch(
+    apiURL,
+    {
+      headers: { Authorization: `Client-ID ${apiKey}` },
+    },
+    {
+      mode: "no-cors",
+    }
+  );
   const formattedResponse = await response.json();
   return formattedResponse;
 });
 
-// crateSlice contains everything you need
 export const gallerySlice = createSlice({
   name: "gallery",
   initialState: {
     photos: [],
     isLoading: false,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getPhotos.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getPhotos.fulfilled, (state, action) => {
+        state.photos = action.payload;
+        state.isLoading = false;
+      })
+      .addCase(getPhotos.rejected, (state) => {
+        state.isLoading = false;
+      });
   },
 });
 
